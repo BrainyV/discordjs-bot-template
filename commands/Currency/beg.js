f650b3f2e522c187f5f553daca59dfb0798437c5
BrainyV <76687324+BrainyV@users.noreply.github.com>
2021-06-24 21:44:18 +0800

    test

---
+++ commands/Currency/beg.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "beg",
+    category: "currency",
+    description: "Beg for some money",
+    aliases: ["gimme"],
+    execute: async (client, message, args) => {
+
+        message.reply('Command under development')
+    } 
+}
