63bda8a1f9d400a56e79223daa834e3bb2836c46
BrainyV <76687324+BrainyV@users.noreply.github.com>
2021-06-24 21:41:27 +0800

    Checking github notifs

---
+++ commands/Currency/balance.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "balance",
+    category: "currency",
+    description: "Check your or others balance",
+    aliases: ["bal", "coins", "wallet"],
+    execute: async (client, message, args) => {
+
+        message.reply('Command under development')
+    } 
+}
